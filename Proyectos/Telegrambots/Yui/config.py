8b015cc64db217461b2bef8632754b6e7f11e059
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-07-04 02:45:18 -0300

    Update config.py

--- Proyectos/Telegrambots/Yui/config.py
+++ Proyectos/Telegrambots/Yui/config.py
@@ -1,1 +1,9 @@
 mi_token = "5548588894:AAGfxfzGRBfLe0RKIDAtr3--y6plwoMavYs"
+id_chat = 782950458
+
+papa=["estoy triste", "sad", "triste"]
+feliz=["Feliz", "hoy es un buen dia"]
+
+estadocritico=["un 1", "un 2", "un 3", "un 4", "1", "2", "3", "4"]
+estadoestable=["un 5", "un 6", "un 7", "5", "6", "7"]
+estadopositivo=["un 8", "un 9", "un 10", "8", "9", "10"]
