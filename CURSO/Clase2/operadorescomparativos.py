4993e41d1b7140c4381cd1701dba3801c54a6400
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:37:51 -0300

    Create operadorescomparativos.py

---
+++ CURSO/Clase2/operadorescomparativos.py
@@ -0,0 +1,47 @@
+#Operadores de comparación
+
+
+#Seguramente habrás notado que en los ejemplos previos, ingresamos el valor de la condición de manera explícita,
+#es decir, escribimos True y False posterior a la sentencia if.
+
+#el valor booleano True o False. Este valor booleano se obtiene a partir de una comparación entre datos. Estas comparaciones son las siguientes: 
+
+==  (igualdad)
+
+!=   (desigualdad)
+
+>     (mayor que)
+
+<     (menor que)
+
+>=  (mayor o igual que)
+
+<=  (menor o igual que)
+
+# Operador de igualdad (==)
+3 == 4 # False
+5 == 4 # True
+"Pepe" == "José" # False
+"Pepe" == "Pepe" # True
+
+# Operador de desigualdad (!=)
+4 != 4 # False
+4 != 8 = # True
+
+# Operador mayor que (>)
+6 > 4 # True
+3 > 9 # False
+
+# Operador menor que (>)
+4 < 7 # True
+6 < 5 # False
+
+# Operador mayor o igual que (>=)
+4 >= 5 # False
+4 >= 4 # True
+4 >= 3 # True
+
+# Operador menor o igual que (>=)
+4 <= 5 # True
+4 <= 4 # True
+4 <= 3 # False
