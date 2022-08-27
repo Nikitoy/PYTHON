898b467f677a2c0483d57cc832d15298c137c554
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:18:43 -0300

    Create variables.py

---
+++ CURSO/Clase1/variables.py
@@ -0,0 +1,24 @@
+#Una variable es un contenedor en el cuál podemos guardar un valor y referenciarlo posteriormente. 
+#Las variables como lo dice su nombre pueden ser modificadas o reasignadas.
+
+#Una variable en el momento en el que le asignamos un valor. Por ej:
+
+x = "Pepe"
+y = True
+z = 35
+
+#las variables son como una caja donde vas a almacenar un dato simple.
+
+#Para cambiar el valor de la variable (reasignación) debemos llamarla y volver a asignarle un valor.
+#No hay ninguna restricción a la hora de cambiar el tipo de dato que una variable posee.
+
+x=45
+x="PULL"
+
+#la variable se reasigna con el ultimo dato que le hayas definido
+
+#cuando hagas
+
+print(x)
+
+#te va a imprimir pull simple
