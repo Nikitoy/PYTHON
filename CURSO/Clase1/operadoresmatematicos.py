f78cc0bc108a2c4382b9e6e33fc04004dcc1c3c7
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:21:57 -0300

    Create operadoresmatematicos.py

---
+++ CURSO/Clase1/operadoresmatematicos.py
@@ -0,0 +1,28 @@
+#El lenguaje Python soporta los operadores matemáticos básicos. 
+#Estos operadores son la suma, resta, multiplicación, división y el módulo.
+
+#Suma
+
+print(10 + 5)
+
+#Resta
+
+print(10 - 5) 
+
+#Multiplicación
+
+print(10 * 5) 
+
+#División
+
+print(10 / 5) 
+
+#Módulo
+
+print(10 % 5)
+
+#Cabe destacar que podemos usar variables para realizar estas operaciones (siempre y cuando sean valores numéricos), 
+#esto se debe a que llamar a una variable es acceder al valor de esta.
+
+sun=25+15
+print(sun)
