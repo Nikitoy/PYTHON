6f6b918c9caba6a1b6497c26cf2f92f154061a17
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:26:38 -0300

    Create diagramadeflujo.py

---
+++ CURSO/Clase2/diagramadeflujo.py
@@ -0,0 +1,20 @@
+#un diagrama de flujo es un diagrama que describe un proceso, sistema o algoritmo informático. 
+#Se usan ampliamente en numerosos campos para documentar, estudiar, planificar, mejorar y comunicar procesos 
+#que suelen ser complejos en diagramas claros y fáciles de comprender. Los diagramas de flujo 
+#emplean rectángulos, óvalos, diamantes y otras numerosas figuras para definir el tipo de paso, 
+#junto con flechas conectoras que establecen el flujo y la secuencia.
+
+#seria algo como decir lo siguiente:
+
+#la lampara esta ensendida?
+#no
+#entonces prenderla
+
+#la lampara no encendio
+#cambiar el foco
+
+#si todavia no encendio
+#entonces encufarla
+
+#son una serie de pasos que nos indican que pasos seguir, de manera similar le daremos instrucciones a nuestros
+#programas mediante el codigo de python
