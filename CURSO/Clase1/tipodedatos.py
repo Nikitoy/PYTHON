22f1b8649c0555925bda5a09a7d0fd810471ea1a
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:15:11 -0300

    Create tipodedatos.py

---
+++ CURSO/Clase1/tipodedatos.py
@@ -0,0 +1,21 @@
+#Tipos de datos
+
+#Las variables pueden guardar datos de distinto tipo,
+#los tipos básicos son los los numéricos, los de tipo String (cadenas de caracteres), y los booleanos. Veamos en detalle cada tipo:
+
+#Numéricos
+
+#Los valores numéricos pueden ser enteros o con número decimal, los primeros se conocen como int y los segundos como float.
+
+5 # int
+2.8 # float
+
+#Strings
+#Un tipo de dato String es un tipo de dato que está compuesto por uno o varios caracteres. Los usamos para definir nombres, sustantivos, frases, etc.
+
+"Hola"
+
+#Boolean
+
+#Es un valor de verdad. Puede tener el valor verdadero (True) o falso (False).
+
