d14782a0ecef4e537bfffbcbb87a1a7c0ea680b6
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:43:11 -0300

    Create p_elif.py

---
+++ CURSO/Clase2/p_elif.py
@@ -0,0 +1,10 @@
+#elif
+
+#Vamos a utilizar la palabra reservada elif como una forma de decir "si la condición anterior no fue verdadera, probemos esta nueva condición".
+
+a = 33
+b = 33
+if b > a: #false
+  print("b es mayor que a")
+elif a == b: #true
+  print("a y b son iguales") # ejecutamos esta línea de código
