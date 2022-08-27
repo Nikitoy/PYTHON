6ceb7cb00dd10ba504156f47d927c521db26cb0c
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:33:49 -0300

    Create condicionales.py

---
+++ CURSO/Clase2/condicionales.py
@@ -0,0 +1,29 @@
+#Estructura Condicional IF
+
+#En el caso de que una condición se cumpla, ejecutaremos una o varias sentencias (líneas de código) y seguiremos con la ejecución del programa. 
+#En el caso de que la condición no se cumpla no ejecutaremos dichas sentencias y solo continuaremos con la ejecución de nuestro programa.
+
+#En un lenguaje de programación, decir que una condición se cumple para ejecutar determinadas sentencias, 
+#significa que esa condición es verdadera o en terminos del lenguaje Python , significa que es True.
+
+
+if True:
+  print("La condición se cumplió")
+  
+#python utiliza una sangría o indentación, luego del if dejas un espacio en blanco en la linea de abajo
+#si no te tira error
+
+if False:
+  print("La condición se cumplió")
+  
+#En el caso que la condicion fuera false se ejecutaria la condicion de false y imprimiria el dato.
+
+#tambien esta la condicion else
+
+#si if es si, bueno else es encontes xd
+
+if True:
+  print("I am the soloplayer")
+  else:
+    print("me gusta trabajar en equipo")
+    
