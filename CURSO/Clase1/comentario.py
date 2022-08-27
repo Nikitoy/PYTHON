14fccd1b1540bb59404716aadae10209af0b9a46
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 00:10:11 -0300

    Create comentario.py

---
+++ CURSO/Clase1/comentario.py
@@ -0,0 +1,10 @@
+#Podemos crear comentarios en Python de dos formas:
+#Anteponiendo el símbolo # (numeral) a la línea de texto que representa el comentario.
+
+#Este es un comentario
+print("¡Hola Mundo!")
+
+#print("Hello, World!")
+print("¡Hola Mundo!")
+
+#podemos comentar desde linea de texto, variables, metodos etc, y no afectaran al codigo
