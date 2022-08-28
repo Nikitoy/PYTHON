cc2e24e6ee33f84df2d70f5eff406ba77e892808
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 22:58:01 -0300

    Create tablas.py

---
+++ CURSO/Clase3/tablas.py
@@ -0,0 +1,6 @@
+#Tablas de verdad
+
+
+#Esto lo podemos ver resumido en lo que se conoce como “tablas de verdad”. Veamos el resumen de las operaciones lógicas vistas en una tabla de verdad.
+
+#URL:https://youtu.be/Pfyuv5ZnNNw
