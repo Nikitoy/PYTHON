a3599788a7f64dfb71ca3ef0569bc0e23d613b65
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 23:09:01 -0300

    Update arrays.py

--- CURSO/Clase4/arrays.py
+++ CURSO/Clase4/arrays.py
@@ -22,3 +22,29 @@ print(x)
 
 #las lista las podemos almacenar en variables, pero si queremos imprimir un unico elemento de esa lista, buscamos el indice del elemento en la lista
 #y en la variable ponemos el nombre de la lista y entre [] el numero de indice
+
+
+#Para acceder a la cantidad de elementos que posee el array debemos llamar a una función interna de Python llamada len() que significa lenght (longitud).
+
+len(autos)
+
+esto se hace para saber cuantos elementos hay en esta lista
+
+
+Para agregar un elemento vamos a usar la función append(). La sintaxis es la siguiente:
+  
+  autos.append("Dodge") # ['Chevrolet', 'Volvo', 'BMW', 'Dodge']
+  
+  asi apareceria en la lista
+  
+Para remover elementos lo podemos hacer de 2 maneras, una es usando la función pop()
+
+  autos.pop(1)
+  autos # ['Chevrolet', 'BMW']
+  
+  y se borro el elemento con indice 1
+  
+Otra forma de remover elementos del array es usando la función remove() 
+
+  autos.remove("BMW")
+  autos # ['Chevrolet']
