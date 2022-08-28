69fae7ce63b62a0e119ac201a1aa0e2b273aa793
NizuBW <108291627+NizuBW@users.noreply.github.com>
2022-08-27 23:16:38 -0300

    Create bucles.py

---
+++ CURSO/Clase4/bucles.py
@@ -0,0 +1,22 @@
+#Bucles
+
+#Los bucles nos permiten repetir un bloque de código, una cantidad de veces determinada.
+#En Python existen dos tipos de estructuras de control repetitivas, los bucles while y for.
+
+#Bucle While
+
+#Utilizando el bucle while vamos a poder ejecutar un cierto bloque de código mientras una condición sea igual a True. Veamos un ejemplo:
+  
+  i = 1
+while i < 6:
+  print(i)
+  i = i + 1
+
+# 1
+# 2
+# 3
+# 4
+# 5
+
+#definimos una variable y un bucle que mientras i sea menor a 6 que imprima el valor de i y le sume 1
+#cuando i deje de ser menor a 6 se detendra el bucle
